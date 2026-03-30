648886c6c7cae8b3dc4312c9e8b7d1b24c0ca83e
Witold <145793393+HOTdamianek123@users.noreply.github.com>
2026-03-30 20:07:52 +0200

    Refactor Tetris class methods for clarity

--- tetris.cpp
+++ tetris.cpp
@@ -1,3 +1,4 @@
+#include "tetris.h"
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,7 +18,7 @@ using vi = vector<int>;
 mt19937 gen(chrono::high_resolution_clock::now().time_since_epoch().count());
 
 int los(int l, int r) {
-    return (l + (gen()%(r - l + 1)));
+    return (l + (gen() % (r - l + 1)));
 }
 
 const int W = 10;
@@ -69,113 +70,103 @@ vector<vector<vector<pair<int, int>>>> tetromina = {
     }
 };
 
-struct Tetris {
-    //indeksowanie planszy ofc od zera
-    //dolny lewy to [0][0]
-    int lines; //how many lines erase
-    vector<vi> board;
-    int H, W;
-    int block; //type of current block
-    bool end_of_game;
+void Tetris::init(int h, int w) {
+    H = h;
+    W = w;
+    board = {};
+    board.resize(h, vi(w, 0));
+    block = los(0, sz(tetromina) - 1);
+    end_of_game = false;
+    lines = 0;
+}
 
-    void init(int h, int w) {
-        H = h;
-        W = w;
-        board = {};
-        board.resize(h, vi(w, 0));
-        block = los(0, sz(tetromina) - 1);
-        end_of_game = false;
-        lines = 0;
-    }
+bool Tetris::inn(int y, int x) {
+    if (min(y, x) < 0 || y >= H || x >= W) return false;
+    return true;
+}
 
-    bool inn(int y, int x) { //if cell in board
-        if (min(y, x) < 0 || y >= H || x >= W) return false;
-        return true;
-    }
-
-    void erase_row(vi& a) {
-        sort(all(a));
-        lines += sz(a);
-        a.pb(H + 10);
-        int wsk = 0;
-        f(i, 0, H) {
-            if (a[wsk] == i) {
-                f(j,0,W) {
-                    board[i][j] = 0;
-                }
-                wsk++;
-            } else {
-                if (wsk) {
-                    swap(board[i], board[i - wsk]);
-                }
+void Tetris::erase_row(vi& a) {
+    sort(all(a));
+    lines += sz(a);
+    a.pb(H + 10);
+    int wsk = 0;
+    f(i, 0, H) {
+        if (a[wsk] == i) {
+            f(j, 0, W) {
+                board[i][j] = 0;
+            }
+            wsk++;
+        } else {
+            if (wsk) {
+                swap(board[i], board[i - wsk]);
             }
         }
     }
+}
 
-    void move(int col, int rot) { //column where we drop our tetromino , rotation at which we drop our tetromino
-        rot = rot%(sz(tetromina[block]));
-        int maksx = col;
-        f(i, 0, 4) {
-            maksx = max(maksx, col + tetromina[block][rot][i].nd);
-        }
-        if (maksx >= W) {
-            end_of_game = true;
-            return;
-        }
-        int stop = 0; //gdzie sie zatrzyma
-        for (int h = H - 1; h >= 0; h--) {
-            if (stop) break;
-            int ys = h;
-            int xs = col;
-            tv(ele, tetromina[block][rot]) {
-                int ny = ys + ele.st;
-                int nx = xs + ele.nd;
-                if (ny < 0) {
-                    stop = h + 1;
-                    break;
-                }
-                if (ny >= H) {
-                    continue;
-                }
-                if (board[ny][nx]) {
-                    stop = h + 1;
-                    break;
-                }
-            }
-        }
-        int ys = stop;
+void Tetris::move(int col, int rot) {
+    rot = rot % (sz(tetromina[block]));
+    int maksx = col;
+    f(i, 0, 4) {
+        maksx = max(maksx, col + tetromina[block][rot][i].nd);
+    }
+    if (maksx >= W) {
+        end_of_game = true;
+        return;
+    }
+
+    int stop = 0;
+    for (int h = H - 1; h >= 0; h--) {
+        if (stop) break;
+        int ys = h;
         int xs = col;
-        vi do_usuniecia; //numery wierszy do usuniecia
         tv(ele, tetromina[block][rot]) {
             int ny = ys + ele.st;
             int nx = xs + ele.nd;
-            if (!inn(ny, nx)) {
-                end_of_game = true;
-                return;
+            if (ny < 0) {
+                stop = h + 1;
+                break;
             }
-            board[ny][nx]++;
-            int sum = 0;
-            f(i, 0, W) {
-                sum += board[ny][i];
+            if (ny >= H) {
+                continue;
             }
-            if (sum == W) {
-                do_usuniecia.pb(ny);
+            if (board[ny][nx]) {
+                stop = h + 1;
+                break;
             }
         }
-        erase_row(do_usuniecia);
-        block = los(0, sz(tetromina) - 1);
     }
 
-    void render() {
-        for (int h = H - 1; h >= 0; h--) {
-            f(i, 0, W) {
-                if (board[h][i]) {
-                    cout << '#';
-                } else {
-                    cout << '.';
-                }
-            }
-            cout << "\n";
+    int ys = stop;
+    int xs = col;
+    vi do_usuniecia;
+    tv(ele, tetromina[block][rot]) {
+        int ny = ys + ele.st;
+        int nx = xs + ele.nd;
+        if (!inn(ny, nx)) {
+            end_of_game = true;
+            return;
+        }
+        board[ny][nx]++;
+        int sum = 0;
+        f(i, 0, W) {
+            sum += board[ny][i];
+        }
+        if (sum == W) {
+            do_usuniecia.pb(ny);
         }
     }
-};
+
+    erase_row(do_usuniecia);
+    block = los(0, sz(tetromina) - 1);
+}
+
+void Tetris::render() {
+    for (int h = H - 1; h >= 0; h--) {
+        f(i, 0, W) {
+            if (board[h][i]) cout << '#';
+            else cout << '.';
+        }
+        cout << "\n";
+    }
+}
